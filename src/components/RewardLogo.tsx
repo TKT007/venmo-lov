a107b9de15466bc03642cbe971d2c9e35a41222e
TKT007 <makosquad.ze@gmail.com>
2025-10-19 00:21:00 -0300

    Update RewardLogo.tsx

--- src/components/RewardLogo.tsx
+++ src/components/RewardLogo.tsx
@@ -9,7 +9,7 @@ export const RewardLogo = ({ className }: RewardLogoProps) => {
     <div className={cn("flex items-center justify-center", className)}>
       <div className="relative">
         <img 
-          src="/venmo-logo.png" 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Venmo_logo.png/1024px-Venmo_logo.png" 
           alt="Venmo Logo" 
           className="w-16 h-16 rounded-2xl shadow-lg object-cover"
         />
